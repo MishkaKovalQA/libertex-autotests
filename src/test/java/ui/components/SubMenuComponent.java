7a9ae76f428e072d2313a14afe5ee7df228ff13c
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-02-01 01:47:58 +0500

    add SubMenuComponent

---
+++ src/test/java/ui/components/SubMenuComponent.java
@@ -0,0 +1,28 @@
+package ui.components;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+
+public class SubMenuComponent {
+
+    private final SelenideElement submenu;
+
+    public SubMenuComponent(SelenideElement submenu) {
+        this.submenu = submenu;
+    }
+
+    @Step("Check that submenu contains items with texts: {texts}")
+    public SubMenuComponent checkItemsVisible(List<String> texts) {
+        texts.forEach(text ->
+                submenu.$$(".nav-submenu__item")
+                        .findBy(text(text))
+                        .shouldBe(visible)
+        );
+        return this;
+    }
+}

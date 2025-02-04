e75004276003d8de358f51c61194f15ee1ebad72
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-02-05 00:04:54 +0500

    add CareersTests

---
+++ src/test/java/ui/tests/CareersTests.java
@@ -0,0 +1,56 @@
+package ui.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CareersTests extends TestBase {
+
+    @BeforeEach
+    void beforeEach() {
+        careersPage.openCareersPage()
+                .acceptCookie();
+    }
+
+//todo test check careerPageContent
+
+    @Test
+    @DisplayName("Filtering jobs by location should update job count and allow clearing filters")
+    void filteringJobsShouldUpdateJobsCountAndBeResettableTest() {
+        var jobsCountBeforeFiltering = careersPage.getStartJobCount();
+
+        careersPage.verifyClearFiltersButtonNotVisible()
+                .applyLocationFilter("Serbia, Vojvodina, Belgrade")
+                .checkJobResultsCountShouldBeLessThan(jobsCountBeforeFiltering)
+                .clearFilters()
+                .verifyJobCountIs(jobsCountBeforeFiltering);
+    }
+
+    @Test
+    @DisplayName("Check that job title remains the same after opening the job details page")
+    void nameOfJobShouldBeTheSameAfterOpeningTest() {
+        var expectedJobName = careersPage.getNameFromFirstJob();
+        careersPage.openFirstJob()
+                .verifyJobOverviewElements()
+                .checkJobTitleHaveSameTitle(expectedJobName);
+    }
+
+    @Test
+    @DisplayName("Job application page should contain all required elements")
+    void jobApplicationPageShouldContainRequiredElementsTest() {
+        careersPage.openFirstJob()
+                .verifyJobOverviewElements();
+    }
+
+    @Test
+    @DisplayName("Verify clearing the application form results in reset state")
+    void shouldClearResultsInJobApplicationTest() {
+        careersPage.openFirstJob()
+                .openApplicationForm()
+                .checkClearButtonDisabled()
+                .fillPersonalInformationForm()
+                .clearFormByClearButton()
+                .checkClearButtonDisabled()
+                .verifyFormCleared();
+    }
+}

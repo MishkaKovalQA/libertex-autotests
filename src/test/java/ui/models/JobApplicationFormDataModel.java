e6159eec4097e5f6485afbf6f00d48881533c947
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-02-05 02:07:22 +0500

    add javafaker generations for input data

---
+++ src/test/java/ui/models/JobApplicationFormDataModel.java
@@ -0,0 +1,31 @@
+package ui.models;
+
+import com.github.javafaker.Faker;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Locale;
+
+@Data
+@Builder
+public class JobApplicationFormDataModel {
+
+    private static Faker faker() {
+        return Faker.instance(Locale.ENGLISH);
+    }
+
+    @Builder.Default
+    private String firstName = faker().name().firstName();
+
+    @Builder.Default
+    private String lastName = faker().name().lastName();
+
+    @Builder.Default
+    private String email = faker().internet().emailAddress();
+
+    @Builder.Default
+    private String phone = faker().phoneNumber().subscriberNumber(10);
+
+    @Builder.Default
+    private String avatarFileName = "kitten.jpg";
+}
--- src/test/java/ui/pages/careers/JobApplicationPage.java
+++ src/test/java/ui/pages/careers/JobApplicationPage.java
@@ -2,6 +2,7 @@ package ui.pages.careers;
 
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
+import ui.models.JobApplicationFormDataModel;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Condition.empty;
@@ -9,14 +10,14 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class JobApplicationPage {
 
-    private final SelenideElement clearPersonalInformationButton = $("[data-ui=clear-section-0]");
-    private final SelenideElement firstNameInput = $("#firstname");
-    private final SelenideElement lastNameInput = $("#lastname");
-    private final SelenideElement emailInput = $("#email");
-    private final SelenideElement phoneInput = $("[name=phone]");
-    private final SelenideElement avatarUpload = $("[data-ui=avatar]");
-    private final SelenideElement uploadAvatarButton = $("[data-ui=crop-image]");
-    private final SelenideElement uploadedImagePreview = $("[data-role=preview] img");
+    private final SelenideElement clearPersonalInformationButton = $("[data-ui=clear-section-0]"),
+                                  firstNameInput = $("#firstname"),
+                                  lastNameInput = $("#lastname"),
+                                  emailInput = $("#email"),
+                                  phoneInput = $("[name=phone]"),
+                                  avatarUpload = $("[data-ui=avatar]"),
+                                  uploadAvatarButton = $("[data-ui=crop-image]"),
+                                  uploadedImagePreview = $("[data-role=preview] img");
 
     @Step("Check that clear button is disabled")
     public JobApplicationPage checkClearButtonDisabled() {
@@ -27,12 +28,14 @@ public class JobApplicationPage {
 
     @Step("Fill out application form")
     public JobApplicationPage fillPersonalInformationForm() {
-        firstNameInput.setValue("abc");
-        lastNameInput.setValue("def");
-        emailInput.setValue("def@gmail.com");
-        phoneInput.setValue("9538024156");
+        var formData = JobApplicationFormDataModel.builder().build();
 
-        var avatar = "kitten.jpg";
+        firstNameInput.setValue(formData.getFirstName());
+        lastNameInput.setValue(formData.getLastName());
+        emailInput.setValue(formData.getEmail());
+        phoneInput.setValue(formData.getPhone());
+
+        var avatar = formData.getAvatarFileName();
         uploadAvatar(avatar);
         verifyUploadedFile(avatar);
 

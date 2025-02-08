1dc665d787b95a378eec3c981d22dcb75307f595
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-02-08 21:41:19 +0500

    get back without version

--- src/test/java/ui/config/SelenideConfig.java
+++ src/test/java/ui/config/SelenideConfig.java
@@ -15,10 +15,6 @@ public interface SelenideConfig extends Config {
     @DefaultValue("CHROME")
     String getBrowser();
 
-    @Key("version")
-    @DefaultValue("128.0")
-    String getBrowserVersion();
-
     @Key("remoteUrl")
     String getRemoteUrl();
 }
